c21fda255e6f3f38acb48568e97ce30170c5b726
Keso Rupert <Rupert.Keso@kpmg.nl>
2023-12-18 12:08:56 +0100

    Finally finished day3 part2

--- day3/part2.py
+++ day3/part2.py
@@ -5,9 +5,9 @@ schematic = []
 i = 0
 
 partsSymbols = ["*", "#", "+", "$", "=", "=", "/", "@", "%", "&", "-"]
-partNumbersSum = 0
+partNumbersMultiplied = 0
 foundNumbers = []  # Array of found numbers, consisting of the number and the index (123, (1,2))
-foundParts = []  # Same for the parts
+numbersAndGears = []  # Array of numbers that are adjecent to gears
 
 # First build the matrix
 for line in inputFile:
@@ -29,38 +29,49 @@ for i in range(0, len(schematic)):
             if currentDigit:  # This means we reached the end of a digit and need to store it and its location
                 foundNumbers.append((currentDigit, (i, j-len(currentDigit))))
                 currentDigit = ""
-
-            # if not currentChar == ".":
-            #     foundParts.append((currentChar, (i, j))) # tuple of part and index, like ("*"", (1,2))
         elif currentChar.isdigit():
             currentDigit += currentChar  # Add found digit to the currentDigit
     if currentChar:  # Quite literal edge case where the number is on the edge of the matrix
         foundNumbers.append((currentDigit, (i, j-len(currentDigit))))
 
 
-"""
-Ok what do we need to check?
-I need to check every field around the number.
-If the number is 123, on coordinates 8,9, then on the x axis, I need to check 7, 8, 9. (-1, 0, +1)
-On the y axis, I need to check 8, but then consider the length of the string, so that would be 12 (so -1, +3)
-THEN I need to consider the bounds of the matrix, which is len(schematic[i]) and len(schematic[i][j]) if I'm not mistaken.
-x cannot be higher then the first, y cannot be higher then the second
-"""
-def checkForAdjecentParts(number):
+def checkForAdjecentGears(number):
     x, y = number[1][0], number[1][1]
 
     for i in range(x-1, x+2):  # Do I need to +2 because range is not inclusive?
         for j in range(y-1, (y+len(number[0])+1)):  # Took me way too long to figure out this should only be a +1
             if i < 0 or j < 0 or i >= len(schematic) or j >= len(schematic):  # Consider the bounds of the matrix
                 continue
-            if schematic[i][j] in partsSymbols:
-                return True
+            if schematic[i][j] == "*":
+                return (i, j)
 
-    return False
+    return None
+
+
+def checkNumbersAndGears():
+    global partNumbersMultiplied
+    coordinateCount = {}
+
+    for number, gearCoords in numbersAndGears:
+        if gearCoords in coordinateCount:
+            coordinateCount[gearCoords] += 1
+        else:
+            coordinateCount[gearCoords] = 1
+    
+    for coord, count in coordinateCount.items():
+        result = 1
+        if count == 2:
+            for number, gearCoords in numbersAndGears:
+                if gearCoords == coord:
+                    result *= int(number)
+            partNumbersMultiplied += result
 
 
 for number in foundNumbers:
-    if checkForAdjecentParts(number):
-        partNumbersSum += int(number[0])
+    gearCoordinates = checkForAdjecentGears(number)
+    if gearCoordinates:
+        numbersAndGears.append((number[0], gearCoordinates))
 
-print(partNumbersSum)
+
+checkNumbersAndGears()
+print(partNumbersMultiplied)
