3fdf1958ae789284ae3d9d00755097ef62355483
Keso Rupert <kesorupert@hotmail.com>
2024-08-27 10:46:13 +0200

    Finished day 6

--- 2015/src/day6/Day6.java
+++ 2015/src/day6/Day6.java
@@ -2,25 +2,27 @@ package day6;
 
 import util.InputReader;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class Day6 {
 
     static Set<Coords> turnedOnLights = new HashSet<>();
+    static Map<Coords, Integer> lightsAndBrightness = new HashMap<Coords, Integer>();
 
     public static void main(String[] args) {
         InputReader reader = new InputReader("2015/src/day6/input.txt");
 
         solvePart1(reader.getInputStreamForLines());
-//        solvePart2(reader.getInputStreamForLines());
+        solvePart2(reader.getInputStreamForLines());
     }
 
 
     private static void solvePart1(Stream<String> lines) {
-
 
         for (String line : (Iterable<String>) lines::iterator) {
             // Sneaky replace to make sure amount of words is the same for the input lines
@@ -63,25 +65,42 @@ public class Day6 {
         return new Coords(endingX, endingY);
     }
 
-    private static void turnOnLightAtCoords(Coords startingCoords, Coords endingCoords) {
-        for (int x = startingCoords.x(); x <= endingCoords.x(); x++) {
-            for (int y = startingCoords.y(); y <= endingCoords.y(); y++) {
-                turnedOnLights.add(new Coords(x, y));
-            }
-        }
-    }
-
-    private static void turnOffLightAtCoords(Coords startingCoords, Coords endingCoords) {
-        for (int x = startingCoords.x(); x <= endingCoords.x(); x++) {
-            for (int y = startingCoords.y(); y <= endingCoords.y(); y++) {
-                turnedOnLights.remove(new Coords(x, y));
-            }
-        }
-    }
-
     private static void solvePart2(Stream<String> lines) {
 
+        for (String line : (Iterable<String>) lines::iterator) {
+            line = line.replace("turn on", "turnon").replace("turn off", "turnoff");
 
+            Coords startingCoords = getStartingCoords(line);
+            Coords endingCoords = getEndingCoords(line);
+
+            for (int x = startingCoords.x(); x <= endingCoords.x(); x++) {
+                for (int y = startingCoords.y(); y <= endingCoords.y(); y++) {
+                    Coords currentCoords = new Coords(x, y);
+                    if (line.startsWith("turnon")) {
+                        if (lightsAndBrightness.containsKey(currentCoords)) {
+                            lightsAndBrightness.put(currentCoords, lightsAndBrightness.get(currentCoords) + 1);
+                        } else {
+                            lightsAndBrightness.put(currentCoords, 1);
+                        }
+                    } else if ( line.startsWith("turnoff")){
+                        if (lightsAndBrightness.containsKey(currentCoords)) {
+                            lightsAndBrightness.put(currentCoords, lightsAndBrightness.get(currentCoords) - 1);
+                            if (lightsAndBrightness.get(currentCoords) < 0)  {
+                                lightsAndBrightness.put(currentCoords, 0);
+                            }
+                        }
+                    } else {
+                        // Toggle behaviour
+                        if (lightsAndBrightness.containsKey(currentCoords)) {
+                            lightsAndBrightness.put(currentCoords, lightsAndBrightness.get(currentCoords) + 2);
+                        } else {
+                            lightsAndBrightness.put(currentCoords, 2);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(lightsAndBrightness.values().stream().reduce(0, Integer::sum));
     }
 }
 
